e6ef872f1fadb54231af4e7b6129733db140dc24
Saheb Preet Singh <saheb@propertyguru.com.sg>
2019-06-14 02:14:47 +0800

    added install on homescreen

--- public/service-worker-base.js
+++ public/service-worker-base.js
@@ -32,10 +32,26 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
-self.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  return false;
+/**
+ * Handling Offline Page fallback
+ */
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+            fetch(event.request.url).catch(error => {
+                // Return the offline page
+                return caches.match('/static/');
+            })
+        );
+    }
+    else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
 
 workbox.precaching.precacheAndRoute([], {});
